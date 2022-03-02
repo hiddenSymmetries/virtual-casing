54a5d8aff6435fdc34fa57f152611794da5d36d5
Bharat Medasani <mbkumar@gmail.com>
2022-03-02 14:02:01 -0500

    Fixed compilation issues

--- src/python.cpp
+++ src/python.cpp
@@ -3,28 +3,22 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <pybind11/stl_bind.h>
 
 namespace py = pybind11;
+
 
 template<class Real>
 class PyVirtualCasing : public VirtualCasing<Real> {
 public:
     using VirtualCasing<Real>::VirtualCasing;
-    // using VirtualCasing<Real>::ComputeBext;
-    void set_surface(long Nt, long Np, const std::vector<Real>& X){ // Using python style method names
-        VirtualCasing<Real>::SetSurface(Nt, Np, sctl::Vector<Real>(X));
-    }
-    /*void set_accuracy(long digits){
-        VirtualCasing::setAccuracy(digits);
-    }*/
+
     std::vector<Real> compute_external_B(const std::vector<Real>& B){
-        sctl::Vector<Real> B_ext;
-        VirtualCasing<Real>::ComputeBext(B_ext, sctl::Vector<Real>(B));
+        std::vector<Real> B_ext;
+        VirtualCasing<Real>::ComputeBext(B_ext, B);
         return B_ext;
     }
-
 };
+
 
 PYBIND11_MODULE(virtual_casing, m) {
     // SCTL Vector class has no duck typing with std::vector. The below two lines won't work
@@ -55,29 +49,8 @@ PYBIND11_MODULE(virtual_casing, m) {
 
     py::class_<PyVirtualCasing<double>, VirtualCasing<double>>(m, "VirtualCasing")
         .def(py::init<>())
-        /*.def("set_surface", [](long Nt, long Np, const vector<double>& X){
-            VirtualCasing<double>.setSurface()
-        }
-        .def("set_accuracy", &VirtualCasing<double>.setAccuracy)
-        .def("compute_external_B", &VirtualCasing<double>.computeBext);
-         */
-        .def("set_surface", &PyVirtualCasing<double>::set_surface)
-        .def("set_accuracy", &VirtualCasing<double>::SetAccuracy);
+        .def("set_surface", &VirtualCasing<double>::SetSurface)
+        .def("set_accuracy", &VirtualCasing<double>::SetAccuracy)
         .def("compute_external_B", &PyVirtualCasing<double>::compute_external_B);
 
 }
-
-
-
-
-
-// biest::SurfType 
-// sctl::Vector
-// VirtualCasingTestData
-// VirtualCasingTestData<Real>::SurfaceCoordinates
-// VirtualCasingTestData<Real>::BFieldData
-// VirtualCasing
-// virtual_casing.SetSurface
-// virtual_casing.SetAccuracy
-// virtual_casing.ComputeBext
-// biest::SurfType::AxisymNarrow
