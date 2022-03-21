ba0ff14b95f32ee4e051302ecfd00378e06c7b9d
Dhairya Malhotra <dmalhotra@flatironinstitute.org>
2022-03-21 14:03:31 -0400

    Fix C interface

--- src/virtual-casing.cpp
+++ src/virtual-casing.cpp
@@ -1,15 +1,15 @@
 #include <virtual-casing.h>
 #include <virtual-casing.hpp>
+#include <tuple>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void GenerateVirtualCasingTestDataF(float* Bext, float* Bint, long Nt, long Np, const float* X) {
-  std::vector<float> Bext_(3*Nt*Np);
-  std::vector<float> Bint_(3*Nt*Np);
+  std::vector<float> Bext_, Bint_;
   const std::vector<float> X_(X, X+3*Nt*Np);
-  VirtualCasingTestData<float>::BFieldData(Bext_, Bint_, Nt, Np, X_);
+  std::tie(Bext_, Bint_) = VirtualCasingTestData<float>::BFieldData(Nt, Np, X_);
   for (long i = 0; i < 3*Nt*Np; i++) {
     Bext[i] = Bext_[i];
     Bint[i] = Bint_[i];
@@ -17,10 +17,9 @@ void GenerateVirtualCasingTestDataF(float* Bext, float* Bint, long Nt, long Np, 
 }
 
 void GenerateVirtualCasingTestDataD(double* Bext, double* Bint, long Nt, long Np, const double* X) {
-  std::vector<double> Bext_(3*Nt*Np);
-  std::vector<double> Bint_(3*Nt*Np);
+  std::vector<double> Bext_, Bint_;
   const std::vector<double> X_(X, X+3*Nt*Np);
-  VirtualCasingTestData<double>::BFieldData(Bext_, Bint_, Nt, Np, X_);
+  std::tie(Bext_, Bint_) = VirtualCasingTestData<double>::BFieldData(Nt, Np, X_);
   for (long i = 0; i < 3*Nt*Np; i++) {
     Bext[i] = Bext_[i];
     Bint[i] = Bint_[i];
@@ -64,17 +63,15 @@ void VirtualCasingSetAccuracyD(int digits, void* ctx) {
 }
 
 void VirtualCasingComputeBextF(float* Bext, const float* B, long Nt, long Np, const void* ctx) {
-  std::vector<float> Bext_(3*Nt*Np);
   const std::vector<float> B_(B, B+3*Nt*Np);
   const VirtualCasing<float>& virtual_casing = *(VirtualCasing<float>*)ctx;
-  virtual_casing.ComputeBext(Bext_, B_);
+  std::vector<float> Bext_ = virtual_casing.ComputeBext(B_);
   for (long i = 0; i < 3*Nt*Np; i++) Bext[i] = Bext_[i];
 }
 void VirtualCasingComputeBextD(double* Bext, const double* B, long Nt, long Np, const void* ctx) {
-  std::vector<double> Bext_(3*Nt*Np);
   const std::vector<double> B_(B, B+3*Nt*Np);
   const VirtualCasing<double>& virtual_casing = *(VirtualCasing<double>*)ctx;
-  virtual_casing.ComputeBext(Bext_, B_);
+  std::vector<double> Bext_ = virtual_casing.ComputeBext(B_);
   for (long i = 0; i < 3*Nt*Np; i++) Bext[i] = Bext_[i];
 }
 
