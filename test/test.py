48c88b26855848248ae71ea008e670cea492d5b9
Matt Landreman <mattland@umd.edu>
2022-05-07 11:54:47 -0400

    Added test

--- test/test.py
+++ test/test.py
@@ -21,7 +21,7 @@ class VirtualCasingTests(unittest.TestCase):
         max_err = np.abs(B_err).max()
         max_rel_err = max_err / max_val
         print(f"For digits = {digits} and half_period = {half_period}, maximum relative error = {max_rel_err}")
-        self.assertLess(max_rel_err, expected_error / 5)
+        self.assertLess(max_rel_err, expected_error)
 
     def test_w7x(self):
         """
@@ -41,6 +41,46 @@ class VirtualCasingTests(unittest.TestCase):
                                      nphi_factor * 6 * digits, 32 * digits,
                                      nphi_factor * 20, 40,
                                      expected_errors[j])
+
+    def test_test_data_phi_grid(self):
+        """
+        For VirtualCasingTestData.surface_coordinates, make sure the phi
+        grid behaves as expected for both half_period=True and False.
+        """
+        for half_period in [True, False]:
+            for surf_type in [vc.SurfType.W7X_]:
+                nfp = 5
+                nphi = 30
+                ntheta = 25
+                x1d = vc.VirtualCasingTestData.surface_coordinates(nfp, half_period, nphi, ntheta, surf_type)
+                x1d = np.array(x1d)
+
+                # Unpack 1D array results:
+                x3d = np.zeros((nphi, ntheta, 3))
+                for jxyz in range(3):
+                    x3d[:, :, jxyz] = x1d[jxyz * nphi * ntheta: (jxyz + 1) * nphi * ntheta].reshape((nphi, ntheta), order='C')
+
+                # Check order:
+                index = 0
+                for jxyz in range(3):
+                    for jphi in range(nphi):
+                        for jtheta in range(ntheta):
+                            np.testing.assert_allclose(x1d[index], x3d[jphi, jtheta, jxyz])
+                            index += 1
+
+                phi_from_vc = np.arctan2(x3d[:, :, 1], x3d[:, :, 0])
+
+                if half_period:
+                    phi1d = np.linspace(0, np.pi / nfp, nphi, endpoint=False)
+                    phi1d += 0.5 * (phi1d[1] - phi1d[0])
+                else:
+                    phi1d = np.linspace(0, 2 * np.pi / nfp, nphi, endpoint=False)
+                theta1d = np.linspace(0, 2 * np.pi, ntheta, endpoint=False)
+
+                theta2d, phi2d = np.meshgrid(theta1d, phi1d)
+
+                np.testing.assert_allclose(phi2d, phi_from_vc, atol=1e-14)
+
 
 if __name__ == "__main__":
     unittest.main()
