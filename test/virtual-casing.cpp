9b65d3f130d56c56d04ecb16736e319bd82e0493
Dhairya Malhotra <dmalhotra@flatironinstitute.org>
2026-02-01 16:50:59 -0500

    Add test for ComputeBextOffSurf in test/virtual-casing.cpp

--- test/virtual-casing.cpp
+++ test/virtual-casing.cpp
@@ -15,9 +15,28 @@ template <class Real> void test(int digits, int NFP, bool half_period, long Nt, 
   //  }
   //}
 
+  const auto print_relative_err = [NFP,half_period,Nt,Np,trg_Nt,trg_Np,&X](const std::string& name, const std::vector<Real>& B, const std::vector<Real>& Bext_ref, const std::vector<Real>& Bext) {
+    auto err = Bext;
+    Real max_err = 0, max_val = 0;
+    for (long i = 0; i < (long)err.size(); i++) err[i] -= Bext_ref[i];
+    for (const auto& x:B  ) max_val = std::max<Real>(max_val,fabs(x));
+    for (const auto& x:err) max_err = std::max<Real>(max_err,fabs(x));
+    std::cout<<"Maximum relative error ("<<name<<"): "<<max_err / max_val<<'\n';
+    //if (err.size()%(trg_Nt*trg_Np)==0) biest::WriteVTK(("err_"+name).c_str(), NFP, half_period, Nt, Np, sctl::Vector<Real>(X), trg_Nt, trg_Np, sctl::Vector<Real>(err));
+  };
+
+  // Setup
+  VirtualCasing<Real> virtual_casing;
+  virtual_casing.Setup(digits, NFP, half_period, Nt, Np, X, src_Nt, src_Np, trg_Nt, trg_Np);
+
+  // Get off-surface target points
+  std::vector<Real> Xtrg_offsurf, Xn = virtual_casing.GetNormal(NFP, half_period, Nt, Np);
+  for (long i = 0; i < (long)X.size(); i++) Xtrg_offsurf.push_back(X[i] - 0.08*Xn[i]);
+
   // Generate B fields for testing virtual-casing principle
   std::vector<Real> B, Bext;
   std::vector<Real> GradB, GradBext;
+  std::vector<Real> B_offsurf, Bext_offsurf;
   { // Set B, Bext
     std::vector<Real> Bint_, Bext_;
     std::tie(Bext, std::ignore) = VirtualCasingTestData<Real>::BFieldData(NFP, half_period, Nt, Np, X, trg_Nt, trg_Np);
@@ -26,41 +45,90 @@ template <class Real> void test(int digits, int NFP, bool half_period, long Nt, 
     B.assign(B_.begin(), B_.end());
   }
   { // Set GradB, GradBext
-    std::vector<Real> GradBint_, GradBext_;
-    std::tie(GradBext, std::ignore) = VirtualCasingTestData<Real>::GradBFieldData(NFP, half_period, Nt, Np, X, trg_Nt, trg_Np);
-    std::tie(GradBext_, GradBint_) = VirtualCasingTestData<Real>::GradBFieldData(NFP, half_period, Nt, Np, X, src_Nt, src_Np);
-    const auto GradB_ = sctl::Vector<Real>(GradBint_) + sctl::Vector<Real>(GradBext_);
+    std::vector<Real> GradBint_;
+    std::tie(GradBext, GradBint_) = VirtualCasingTestData<Real>::GradBFieldData(NFP, half_period, Nt, Np, X, trg_Nt, trg_Np);
+    const auto GradB_ = sctl::Vector<Real>(GradBint_) + sctl::Vector<Real>(GradBext);
     GradB.assign(GradB_.begin(), GradB_.end());
   }
-
-  // Setup
-  VirtualCasing<Real> virtual_casing;
-  virtual_casing.Setup(digits, NFP, half_period, Nt, Np, X, src_Nt, src_Np, trg_Nt, trg_Np);
+  { // Set B_offsurf, Bext_offsurf
+    std::vector<Real> Bint_offsurf_;
+    std::tie(Bext_offsurf, Bint_offsurf_) = VirtualCasingTestData<Real>::BFieldData(NFP, half_period, Nt, Np, X, Xtrg_offsurf);
+    const auto B_offsurf_ = sctl::Vector<Real>(Bint_offsurf_) + sctl::Vector<Real>(Bext_offsurf);
+    B_offsurf.assign(B_offsurf_.begin(), B_offsurf_.end());
+  }
 
   // Compute Bext field
   auto Bext_ = virtual_casing.ComputeBext(B);
-
-  { // print error
-    auto Berr = Bext;
-    Real max_err = 0, max_val = 0;
-    for (long i = 0; i < (long)Berr.size(); i++) Berr[i] -= Bext_[i];
-    for (const auto& x:B   ) max_val = std::max<Real>(max_val,fabs(x));
-    for (const auto& x:Berr) max_err = std::max<Real>(max_err,fabs(x));
-    std::cout<<"Maximum relative error: "<<max_err/max_val<<'\n';
-    //biest::WriteVTK("Berr", NFP, half_period, Nt, Np, sctl::Vector<Real>(X), trg_Nt, trg_Np, sctl::Vector<Real>(Bext_)-sctl::Vector<Real>(Bext));
-  }
+  print_relative_err("Bext", B, Bext, Bext_);
 
   // Compute GradBext field
   auto GradBext_ = virtual_casing.ComputeGradBext(B);
+  print_relative_err("GradBext", GradB, GradBext, GradBext_);
 
-  { // print error
-    auto GradBerr = GradBext;
-    Real max_err = 0, max_val = 0;
-    for (long i = 0; i < (long)GradBerr.size(); i++) GradBerr[i] -= GradBext_[i];
-    for (const auto& x:GradB   ) max_val = std::max<Real>(max_val,fabs(x));
-    for (const auto& x:GradBerr) max_err = std::max<Real>(max_err,fabs(x));
-    std::cout<<"Maximum relative error (GradBext): "<<max_err / max_val<<"\n\n";
-    //biest::WriteVTK("GradBerr", NFP, half_period, Nt, Np, sctl::Vector<Real>(X), trg_Nt, trg_Np, sctl::Vector<Real>(GradBext_)-sctl::Vector<Real>(GradBext));
+  // Compute off-surface error
+  const auto Bext_offsurf_ = virtual_casing.ComputeBextOffSurf(B, Xtrg_offsurf, (half_period?1:2)*800, 800); // max_Nt, max_Np = 800
+  print_relative_err("Bext_offsurf", B_offsurf, Bext_offsurf, Bext_offsurf_);
+  std::cout<<'\n';
+
+  if (0) { // Visualize off-surface evaluation error
+    const long N = 60;
+    const Real L = 1.6, offset[]={5.8,0,0};
+
+    std::vector<Real> Xtrg(3*N*N*N); // N x N cube of target points
+    for (long i = 0; i < N; i++) {
+      for (long j = 0; j < N; j++) {
+        for (long k = 0; k < N; k++) {
+          const long N3 = N*N*N;
+          const long idx = (i*N+j)*N+k;
+          Xtrg[0*N3+idx] = ((i/(Real)(N-1))-0.5)*L + offset[0];
+          Xtrg[1*N3+idx] = ((j/(Real)(N-1))-0.5)*L + offset[1];
+          Xtrg[2*N3+idx] = ((k/(Real)(N-1))-0.5)*L + offset[2];
+        }
+      }
+    }
+
+    std::vector<Real> Bext_offsurf; // reference data
+    std::tie(Bext_offsurf, std::ignore) = VirtualCasingTestData<Real>::BFieldData(NFP, half_period, Nt, Np, X, Xtrg);
+    const auto Bext_offsurf_ = virtual_casing.ComputeBextOffSurf(B, Xtrg, (half_period?1:2)*400, 400); // max_Nt, max_Np = 400
+
+    { // Write VTK
+      sctl::VTUData vtu_data;
+      for (long i = 0; i < N; i++) {
+        for (long j = 0; j < N; j++) {
+          for (long k = 0; k < N; k++) {
+            const long N3 = N*N*N;
+            const long idx = (i*N+j)*N+k;
+            vtu_data.coord.PushBack((float)Xtrg[0*N3+idx]);
+            vtu_data.coord.PushBack((float)Xtrg[1*N3+idx]);
+            vtu_data.coord.PushBack((float)Xtrg[2*N3+idx]);
+
+            vtu_data.value.PushBack((float)(Bext_offsurf_[0*N3+idx]-Bext_offsurf[0*N3+idx]));
+            vtu_data.value.PushBack((float)(Bext_offsurf_[1*N3+idx]-Bext_offsurf[1*N3+idx]));
+            vtu_data.value.PushBack((float)(Bext_offsurf_[2*N3+idx]-Bext_offsurf[2*N3+idx]));
+          }
+        }
+      }
+      for (long i = 0; i < N-1; i++) {
+        for (long j = 0; j < N-1; j++) {
+          for (long k = 0; k < N-1; k++) {
+            auto idx = [N](long i, long j, long k) {
+              return (i*N+j)*N+k;
+            };
+            vtu_data.connect.PushBack(idx(i+0,j+0,k+0));
+            vtu_data.connect.PushBack(idx(i+0,j+0,k+1));
+            vtu_data.connect.PushBack(idx(i+0,j+1,k+1));
+            vtu_data.connect.PushBack(idx(i+0,j+1,k+0));
+            vtu_data.connect.PushBack(idx(i+1,j+0,k+0));
+            vtu_data.connect.PushBack(idx(i+1,j+0,k+1));
+            vtu_data.connect.PushBack(idx(i+1,j+1,k+1));
+            vtu_data.connect.PushBack(idx(i+1,j+1,k+0));
+            vtu_data.offset.PushBack(vtu_data.connect.Dim());
+            vtu_data.types.PushBack(12);
+          }
+        }
+      }
+      vtu_data.WriteVTK("Bext_offsurf_err", sctl::Comm::Self());
+    }
   }
 }
 
